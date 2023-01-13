a4f854b2e91bcd36e1e692edeff7efc8710cd8a2
prosper <edwardprosper001@gmail.com>
2023-01-13 22:15:47 +0100

    'railway'

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'mynotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+DATABASES = {     
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'sTBTmSzxESLWHzPRJjAx',
+        'HOST': 'containers-us-west-185.railway.app',
+        'PORT': '7452'  
+    } 
+} 
 
 
 # Password validation
