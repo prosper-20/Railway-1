f144fcfdb4e5469d76153971bb0bbf298b518089
prosper <edwardprosper001@gmail.com>
2023-01-14 10:54:05 +0100

    'railway'

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from decouple import config
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'mynotes.wsgi.application'
 # }
 
 
-VALUE = config("PASSWORD")
+VALUE = os.environ.get("PASSWORD")
 
 DATABASES = {     
     'default': {
